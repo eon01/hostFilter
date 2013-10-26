5141d70dd5faeeb3843a317e90fa38e0648f41ba
eon01 <amri.aymen@gmail.com>
2013-10-26 19:46:23 +0200

    Script is working fine

--- hostFilter.sh
+++ hostFilter.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -      
 ###name			:hostFilter : Protect your computer and block undesired content.
 ###description		:hostFilter is a bash script used to prevent your computer from connecting to some websites and servers that are known to contain malicious content, including viruses, spyware, Trojans, web pages that engage in phishing attacks, spam, phorm, shocking or violent content, prevent annoying pop-ups, block ad servers..etc. This will reduce the bandwidth use and protect your computer from malicious softwares. Please forward any suggestion by email to amri.aymen@gmail.com
+###usage		:hostFilter.sh [OPTION]
 #			-h --help : Display this message.
 #			-i : Run the script.
-###usage		:hostFilter.sh [OPTION]
 ###notes           	:First chmod +x this script, install Sed (http://www.gnu.org/software/sed/ ), and run it as a root.
 ###author		:amri.aymen@gmail.com
 ###website		:https://github.com/eon01/hostFilter
